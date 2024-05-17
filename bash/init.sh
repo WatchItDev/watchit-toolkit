2480443fd019f642b570c5ec7def1aa3b4767718
geolffreym <gmjun2000@gmail.com>
2024-05-17 10:31:19 -0600

    first

--- bash/init.sh
+++ bash/init.sh
@@ -52,4 +52,4 @@ ipfs config Datastore.StorageMax "3000GB"
 ipfs config Datastore.StorageGCWatermark 99 --json
 ipfs config Pubsub.Router "gossipsub"
 ipfs config --json Swarm.DisableBandwidthMetrics false
-ipfs daemon
+ipfs daemon &
