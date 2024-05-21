6f8e6a1a13ad4b906e163bbe1e64e5d77019b978
geolffreym <gmjun2000@gmail.com>
2024-05-21 10:33:05 -0600

    init s3 storage

--- bash/pinner.sh
+++ bash/pinner.sh
@@ -5,11 +5,15 @@ jq --compact-output '.manifest[]' $1 | while read i; do
     medium=$(jq --raw-output '.images.medium' <<<$i)
     large=$(jq --raw-output '.images.large' <<<$i)
     
-    ipfs pin add $video
+    
     ipfs pin add $data
+    echo "Pinned data "
     ipfs pin add $small
     ipfs pin add $medium
     ipfs pin add $large
+    echo "Pinned images"
+    ipfs pin add $video
+    echo "Pinned video"
 
     echo "Finished pinning"
 
