980199e4600cc4b90730836e3d85788a3d48041f
Geolffrey Mena <gmjun2000@gmail.com>
2024-06-10 17:44:53 -0600

    fix: added webtransport append

--- bash/init.sh
+++ bash/init.sh
@@ -51,7 +51,7 @@ ipfs config Addresses.Swarm '[
        "/ip4/0.0.0.0/udp/4001/quic-v1",
        "/ip4/0.0.0.0/udp/4001/quic-v1/webtransport",
        "/ip6/::/udp/4001/quic-v1",
-       "/ip4/0.0.0.0/udp/4002/webrtc-direct",
+      
        "/ip6/::/udp/4001/quic-v1/webtransport"
 ]' --json
 
@@ -60,7 +60,7 @@ ipfs config Addresses.AppendAnnounce "[
        \"/ip4/$ip/udp/4001/quic\",
        \"/ip4/$ip/udp/4001/quic-v1\",
        \"/ip4/$ip/udp/4001/quic-v1/webtransport\",
-       \"/ip4/$ip/udp/4002/webrtc-direct\"
+
 ]" --json
 
 ipfs config Swarm.AddrFilters '[
