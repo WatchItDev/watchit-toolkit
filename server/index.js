85477db95a26297b36ad364e5ae9d37d24986a4d
Geolffrey Mena <gmjun2000@gmail.com>
2024-11-23 10:44:28 -0600

    chore: private note settings

---
+++ server/index.js
@@ -0,0 +1,7 @@
+// nonce
+// authorize
+
+// obtener la firma erc721
+// validar la firma, debe contener el contenido y el nonce
+// despues de validarla actualizar el nonce y finalmente conseceder el accesso
+// se debe obtener el cid desde el vault y pasarlo a IPFS para hacer el proxy redirection 
